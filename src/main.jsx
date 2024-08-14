cd511a2c6bf70122c8bb1545a7bfdf6dbac51d91
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-14 18:45:22 -0300

    Link & useNavigate

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './Basis/App.jsx'
+// import App from './Basis/App.jsx'
+import AppLink from './Link/AppLink.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppLink />
   </StrictMode>,
 )
